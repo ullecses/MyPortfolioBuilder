8a4bb81d36a7c540d9044c5fc3c755ace33e5336
Ullecses <ulyana.vorobjova@gmail.com>
2024-09-24 18:16:34 +0300

    Added the project

---
+++ src/main/java/com/example/MyPortfolioBuilder/MyPortfolioBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyPortfolioBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortfolioBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortfolioBuilderApplication.class, args);
+	}
+
+}
