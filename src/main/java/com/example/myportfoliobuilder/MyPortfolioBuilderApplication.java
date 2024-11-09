ee9f5d2b29c7a25d90ac693ba8e7c0d445f5d25a
Ilys Kochev <kochev0987@gmail.com>
2024-11-09 16:50:56 +0300

    Merged Branch with Logger

--- src/main/java/com/example/myportfoliobuilder/MyPortfolioBuilderApplication.java
+++ src/main/java/com/example/myportfoliobuilder/MyPortfolioBuilderApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyPortfolioBuilderApplication {
-
 	public static void main(String[] args) {
 		DatabaseConnection.connect();
 		SpringApplication.run(MyPortfolioBuilderApplication.class, args);
